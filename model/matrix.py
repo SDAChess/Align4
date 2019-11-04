4a3c8f276cf69fae0006296515582b1198e1ea8e
Simon Scatton <44714756+sdachess@users.noreply.github.com>
2019-11-04 15:33:19 +0000

    began matrix class and rules class
    Signed-off-by: Simon Scatton <simon.scatton@outlook.fr>

---
+++ model/matrix.py
@@ -0,0 +1,9 @@
+class Matrix:
+    def __init__(self, size, default_value=None):
+        self.size = size
+        self.matrix = create_matrix(size)
+
+
+    def create_matrix(self, size):
+        for i in
+
