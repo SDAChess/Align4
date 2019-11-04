9fd651ef1494bba1941078d0cba62824c0b59da4
Simon Scatton <simon.scatton@outlook.fr>
2019-11-04 16:08:20 +0000

    Added create_matrix methods
    Signed-off-by: Simon Scatton <simon.scatton@outlook.fr>

--- model/matrix.py
+++ model/matrix.py
@@ -1,9 +1,13 @@
 class Matrix:
     def __init__(self, size, default_value=None):
         self.size = size
-        self.matrix = create_matrix(size)
+        self.create_matrix(default_value)
 
+    def create_matrix(self, default_value):
+        for i in range(self.size):
+            self.matrix.append([])
+            for j in range(self.size):
+                self.matrix[-1].append(default_value)
 
-    def create_matrix(self, size, default_value):
-        for i in size
-
+    def getCell(self, x, y):
+        return self[x][y]
